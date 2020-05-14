2ce2bc466caa009adcbc20ad9cc5d7e83fdcde97
Dileep T Paul <dileeptpaul2019@gmail.com>
2020-05-14 07:55:34 +0530

    wishes

--- FirstJenkin.java
+++ FirstJenkin.java
@@ -1,6 +1,6 @@
 class FirstJenkin{
 public static void main(String u[]){
-System.out.println("Welcome to jenkin");
-System.out.println("Senorah---");
+System.out.println("Good Morning DIleep");
+System.out.println("Have a nice day---");
 }
 }
